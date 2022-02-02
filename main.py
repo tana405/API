b199a3ee0af7ee2c29ad810d54703f38e8804633
Anna Trunova <trunova.am@yandex.ru>
2022-02-02 17:37:29 +0500

    add window

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('s[')
+import pygame
+
+
+class Board:
+    # создание поля
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('Инициализация API')
+    size = width, height = 600, 600
+    screen = pygame.display.set_mode(size)
+
+    board = Board(5, 5)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+    pygame.quit()
+
