94ec0551e8beda15e3d007382bd0a7b42da44a62
Anna Trunova <trunova.am@yandex.ru>
2022-02-02 18:12:46 +0500

    add ll_format

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
 import sys
 import os
+import request
 
-'''https://yandex.ru/maps/53/kurgan/?ll=65.369564%2C55.439279&z=13.57'''
+'''https://static-maps.yandex.ru/1.x/?ll=65.369564,55.439279&spn=0.05045,0.00619&l=map'''
 
 
 class Map:
@@ -12,7 +13,20 @@ class Map:
         self.height = height
         self.coords1 = 65.369564
         self.coords2 = 55.439279
-        self.coords3 = 13.57
+        self.coords3 = 0.05045
+        self.coords4 = 0.00619
+        self.type = "map"
+
+    def ll_format(self):
+        v = 'https://static-maps.yandex.ru/1.x/?ll={0},{1}&spn={2},{3}&l={4}'.format(str(self.coords1),
+                                                                                     str(self.coords2),
+                                                                                     str(self.coords3),
+                                                                                     str(self.coords4),
+                                                                                     self.type)
+        return v
+
+    def load_map(self):
+        pass
 
 
 if __name__ == '__main__':
@@ -30,4 +44,3 @@ if __name__ == '__main__':
         screen.fill((0, 0, 0))
         pygame.display.flip()
     pygame.quit()
-
